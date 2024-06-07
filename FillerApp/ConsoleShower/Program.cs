87e1e7895fb61d75bc76e7fa2819a0cbd4e8fde0
Alexey_Dzhevello <fishwere@mail.ru>
2024-06-07 23:30:30 +0500

    add shuffle mode

--- FillerApp/ConsoleShower/Program.cs
+++ FillerApp/ConsoleShower/Program.cs
@@ -1,5 +1,6 @@
 ﻿using FillerApp;
 using System.Collections.Frozen;
+using System.Security.Cryptography;
 
 namespace ConsoleShower
 {
@@ -9,7 +10,40 @@ namespace ConsoleShower
         {
             var filepath = Constants.HistoryBasePath;
             var taskList = Utils.ReadTasksFromFile(filepath);
-            ShowAll(taskList);
+            Start(taskList);
+        }
+
+        public static void Start(List<FillerApp.Task> taskList)
+        {
+            Console.WriteLine("choose mode: 'nonshuffle' 'shuffle'");
+            var mode = Console.ReadLine();
+            if (mode == "shuffle")
+            {
+                Shuffle(taskList);
+                ShowAll(taskList);
+                return;
+            }
+            if (mode == "nonshuffle")
+            {
+                ShowAll(taskList, 0);
+                return;
+            }
+
+            throw new Exception($"mode {mode} is invalid");
+        }
+
+        private static Random rng = new Random();
+        public static void Shuffle<T>(IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                var k = rng.Next(n + 1);
+                var value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
         }
 
         public static void ShowAll(List<FillerApp.Task> tasks, int startIndex = 0)
@@ -30,15 +64,19 @@ namespace ConsoleShower
                     Console.WriteLine(rightAns);
                 }
                 Console.WriteLine($"++++++ {rightAnsCow} / {i + 1 - startIndex} wich is {100 * rightAnsCow / (i + 1.0 - startIndex)} %   +++++");
+                Console.Write("press any button to continue");
                 Console.ReadKey();
+                Console.WriteLine();
             }
         }
 
         private static void RenderTask(FillerApp.Task task)
         {
+            Console.WriteLine();
             Console.WriteLine("-=-=-=-=-=--=-=-=-=-=--=-=-=--=-=-=--=-==-==-=-=-==-=-=-=-=-=");
             Console.WriteLine(task.TaskText);
             Console.WriteLine("------- Options: --------");
+            Shuffle(task.AnswerVariants);
             for (int j = 0; j < task.AnswerVariants.Length; j++)
             {
                 Console.WriteLine($"{j} : {task.AnswerVariants[j]}");
@@ -63,10 +101,10 @@ namespace ConsoleShower
 
                 if (ans.SetEquals(task.TrueAnswers.ToFrozenSet()))
                 {
-                    Console.WriteLine("full ans +1");
+                    Console.WriteLine("------------------------------------------------full ans +1");
                     return true;
                 }
-                Console.WriteLine("wrong");
+                Console.WriteLine("------------------------------------------------wrong");
                 return false;
             }
             catch (Exception ex)
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # boring_online_cource_helper
 want to learn test with cards
+
+
+для использования необходимо поменять в FillerApp/Constants   RootPath  на тот, который у вас
+
+запускать ConsoleShower
+
